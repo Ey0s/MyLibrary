30156fd018f24e9ce039bcf8ce2c1dc914d36ae4
Eyosyas Yoseph <155167032+Ey0s@users.noreply.github.com>
2025-05-31 10:25:03 +0300

    Add files via upload

---
+++ Libraryproject/Libraryproject/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Libraryproject
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.BackColor = System.Drawing.Color.White;
+            this.panelLogin.Controls.Add(this.checkBox1);
+            this.panelLogin.Controls.Add(this.lblErrorMessage);
+            this.panelLogin.Controls.Add(this.btnLogin);
+            this.panelLogin.Controls.Add(this.txtPassword);
+            this.panelLogin.Controls.Add(this.lblPassword);
+            this.panelLogin.Controls.Add(this.txtUsername);
+            this.panelLogin.Controls.Add(this.lblUsername);
+            this.panelLogin.Controls.Add(this.lblTitle);
+            this.panelLogin.Location = new System.Drawing.Point(200, 75);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(400, 350);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorMessage.Location = new System.Drawing.Point(25, 230);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(350, 30);
+            this.lblErrorMessage.TabIndex = 5;
+            this.lblErrorMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblErrorMessage.Visible = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(17)))), ((int)(((byte)(203)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(25, 270);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(350, 40);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtPassword.Location = new System.Drawing.Point(25, 190);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(350, 34);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblPassword.Location = new System.Drawing.Point(25, 170);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(97, 28);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtUsername.Location = new System.Drawing.Point(25, 130);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(350, 34);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblUsername.Location = new System.Drawing.Point(25, 110);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(103, 28);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(25, 36);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(350, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Library Management System";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 230);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 29);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.panelLogin);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Library Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Label lblErrorMessage;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ Libraryproject/Libraryproject/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Libraryproject
+{
+    public partial class Form1 : Form
+    {
+        private const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDB;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            ApplyModernStyling();
+        }
+
+        private void ApplyModernStyling()
+        {
+            // Panel styling
+            panelLogin.BackColor = Color.FromArgb(251, 251, 253);
+            panelLogin.BorderStyle = BorderStyle.None;
+
+            // Button styling
+            btnLogin.FlatAppearance.MouseOverBackColor = Color.FromArgb(88, 12, 168);
+            btnLogin.FlatAppearance.MouseDownBackColor = Color.FromArgb(68, 2, 148);
+            btnLogin.Cursor = Cursors.Hand;
+
+            // Textbox styling
+            txtUsername.BorderStyle = BorderStyle.FixedSingle;
+            txtPassword.BorderStyle = BorderStyle.FixedSingle;
+
+            // Label styling
+            lblTitle.Font = new Font("Segoe UI Semibold", 20F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.FromArgb(37, 117, 252);
+
+            // Set rounded corners
+            SetRoundedCorners();
+        }
+
+        private void SetRoundedCorners()
+        {
+            int panelRadius = 15;
+            panelLogin.Region = CreateRoundedRegion(panelLogin.ClientRectangle, panelRadius);
+
+            int buttonRadius = 8;
+            btnLogin.Region = CreateRoundedRegion(btnLogin.ClientRectangle, buttonRadius);
+        }
+
+        private Region CreateRoundedRegion(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+            path.AddArc(rect.Right - radius * 2, rect.Y, radius * 2, radius * 2, 270, 90);
+            path.AddArc(rect.Right - radius * 2, rect.Bottom - radius * 2, radius * 2, radius * 2, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - radius * 2, radius * 2, radius * 2, 90, 90);
+            path.CloseAllFigures();
+            return new Region(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterPanel();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            CenterPanel();
+            this.Invalidate();
+        }
+
+        private void CenterPanel()
+        {
+            panelLogin.Left = (this.ClientSize.Width - panelLogin.Width) / 2;
+            panelLogin.Top = (this.ClientSize.Height - panelLogin.Height) / 2;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Modern gradient background
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.FromArgb(240, 242, 245),  // Light gray-blue
+                Color.FromArgb(255, 255, 255),  // White
+                LinearGradientMode.Vertical))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ShowErrorMessage("Please enter both username and password.");
+                return;
+            }
+
+            if (AuthenticateUser(username, password))
+            {
+                lblErrorMessage.Visible = false;
+                MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.Hide();
+                FormMain mainForm = new FormMain();
+                mainForm.Show();
+            }
+            else
+            {
+                ShowErrorMessage("Invalid username or password.");
+            }
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            lblErrorMessage.Text = message;
+            lblErrorMessage.Visible = true;
+            lblErrorMessage.ForeColor = Color.FromArgb(220, 53, 69); // Bootstrap danger color
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            bool isAuthenticated = false;
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(1) FROM Users WHERE Username = @Username AND PasswordHash = @PasswordHash";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@PasswordHash", password);
+                        int count = (int)command.ExecuteScalar();
+                        isAuthenticated = count > 0;
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    ShowErrorMessage($"Database error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    ShowErrorMessage($"An unexpected error occurred: {ex.Message}");
+                }
+            }
+            return isAuthenticated;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true; 
+            }
+        }
+    }
+}
